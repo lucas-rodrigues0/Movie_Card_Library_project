e03f7a3b5409318667ca7110cf8b1aefc7a28e28
Lucas Rodrigues <lucas.movimento@gmail.com>
2021-03-12 16:42:58 -0300

    Updates

---
+++ precache-manifest.d90ddc86879f23dbb9091feac069f717.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22a0f4f144fe186ac165c4d9c45e76d8",
+    "url": "/Movie_Card_Library_project/index.html"
+  },
+  {
+    "revision": "baa511554c83455de4f7",
+    "url": "/Movie_Card_Library_project/static/css/main.2adb7842.chunk.css"
+  },
+  {
+    "revision": "d7a174e1caec20f7c7f5",
+    "url": "/Movie_Card_Library_project/static/js/2.9d6dda8d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Movie_Card_Library_project/static/js/2.9d6dda8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baa511554c83455de4f7",
+    "url": "/Movie_Card_Library_project/static/js/main.5e0a18ad.chunk.js"
+  },
+  {
+    "revision": "f45d63a3f1114cd5ff94",
+    "url": "/Movie_Card_Library_project/static/js/runtime-main.78bf35e6.js"
+  }
+]);
